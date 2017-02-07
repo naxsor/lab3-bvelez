333f31b2f12a7df87e3d105878a46e097157ede2
naxsor <lemanuel.colon@upr.edu>
2017-02-07 19:41:51 -0400

    Modified windows size, title and location.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,10 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("G.U.I.");
+	    myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(960, 540);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
